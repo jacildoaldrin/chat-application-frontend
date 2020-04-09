7172e1ff913ca16a2727f7bcada718b0f5e8bfe9
Aldrin Jacildo <ajacildo1999@gmail.com>
2020-04-08 22:50:24 -0400

    added server js

---
+++ build/precache-manifest.e054a69c66f1652f68bbf85d612406d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd20cef6e612ef36b37faac7db1510ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "563d4e99848b810b5344",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "45d8cf0689aab0f42215",
+    "url": "/static/css/main.7da3eab8.chunk.css"
+  },
+  {
+    "revision": "563d4e99848b810b5344",
+    "url": "/static/js/2.3ff833de.chunk.js"
+  },
+  {
+    "revision": "a35346a5ff3029a3521395d4c125dfed",
+    "url": "/static/js/2.3ff833de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45d8cf0689aab0f42215",
+    "url": "/static/js/main.e8e3b2ad.chunk.js"
+  },
+  {
+    "revision": "3134d5e4889d6e1df3f1",
+    "url": "/static/js/runtime-main.cfeac9b3.js"
+  }
+]);
