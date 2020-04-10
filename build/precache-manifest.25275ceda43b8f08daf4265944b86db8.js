0f8c04f82cb01386172129e8f8e38c1a22c27e38
Aldrin Jacildo <ajacildo1999@gmail.com>
2020-04-09 23:57:42 -0400

    revert to previous version

---
+++ build/precache-manifest.25275ceda43b8f08daf4265944b86db8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9eeb1bf8ee98843cdeccd8b409891ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06c0e9c7f31423e01ac3",
+    "url": "/static/css/2.35013e12.chunk.css"
+  },
+  {
+    "revision": "3669eb58ef0013f1b2d0",
+    "url": "/static/css/main.a0c17b84.chunk.css"
+  },
+  {
+    "revision": "06c0e9c7f31423e01ac3",
+    "url": "/static/js/2.cf1faa14.chunk.js"
+  },
+  {
+    "revision": "a35346a5ff3029a3521395d4c125dfed",
+    "url": "/static/js/2.cf1faa14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3669eb58ef0013f1b2d0",
+    "url": "/static/js/main.f5a1f127.chunk.js"
+  },
+  {
+    "revision": "3134d5e4889d6e1df3f1",
+    "url": "/static/js/runtime-main.cfeac9b3.js"
+  }
+]);
